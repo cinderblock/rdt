5e4bd8bf65170fe5a52399be9acb8c46b4f4ba60
Cameron Tacklind <cameron@tacklind.com>
2023-03-13 15:39:48 -0700

    Cleanup

--- src/log.ts
+++ src/log.ts
@@ -3,19 +3,16 @@ import winston from 'winston';
 const logger = winston.createLogger({
   level: 'debug',
   format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
+  // defaultMeta: { service: 'user-service' },
   transports: [
-    //
-    // - Write all logs with importance level of `error` or less to `error.log`
-    // - Write all logs with importance level of `info` or less to `combined.log`
-    //
+    // Write all logs with importance level of `error` or less to `error.log`
     new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    // Write all logs with importance level of `info` or less to `combined.log`
     new winston.transports.File({ filename: 'combined.log' }),
   ],
 });
 
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+// Not sure if we'll use `NODE_ENV` long term but for now...
 if (process.env.NODE_ENV !== 'production') {
   logger.add(
     new winston.transports.Console({
