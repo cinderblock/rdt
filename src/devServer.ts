ad59c2155ab56b21b2d1af3dc4e8ccece426d7f5
Cameron Tacklind <cameron@tacklind.com>
2023-05-11 22:20:53 -0700

    Use proxy server
    Needs upgrade to not rely on configuration in user rdt.ts

--- src/devServer.ts
+++ src/devServer.ts
@@ -1,6 +1,8 @@
 import { Target } from './config';
 import esbuild from 'esbuild';
 import logger from './log';
+import { RequestOptions, createServer, request } from 'http';
+import { createProxyServer } from 'http-proxy';
 
 export async function doDevServer(ds: Target['devServer']) {
   if (!ds) return;
@@ -41,11 +43,60 @@ export async function doDevServer(ds: Target['devServer']) {
   try {
     await ctx.watch();
 
-    const { host, port } = await ctx.serve({
+    // Start esbuild server on a random port
+    const { host, port: devServerPort } = await ctx.serve({
       servedir: ds.serveLocal,
     });
 
-    logger.info(`Serving on http://localhost:${port}`);
+    // esbuild defaults to only listen on IPv4 so we can't use "localhost" here
+    const devServerHost = host === '0.0.0.0' ? '127.0.0.1' : host;
+
+    logger.info(`Serving esbuild devServer on http://${devServerHost}:${devServerPort}`);
+
+    // Pick some open port to listen on
+    const proxyServerPort = 9001;
+
+    // Must match the port forward configuration in rdt.ts
+    // TODO: skip the port forward and just use the tunnel server directly
+    const tunnelServerPort = 9080;
+
+    // ssh2 tunnels listen on IPv6 by default
+    // TODO: make this configurable
+    const tunnelServerHost = '::1';
+
+    const proxy = createProxyServer({});
+
+    const proxyServer = createServer({}, (req, res) => {
+      if (req.url === '/ws') {
+        // Forward websocket connections to remote proxy port
+        proxy.web(req, res, {
+          target: {
+            host: tunnelServerHost,
+            port: tunnelServerPort,
+          },
+        });
+      }
+
+      proxy.web(req, res, {
+        target: {
+          host: devServerHost,
+          port: devServerPort,
+        },
+      });
+    });
+
+    proxyServer.on('upgrade', (req, socket, head) => {
+      proxy.ws(req, socket, head, {
+        target: {
+          host: tunnelServerHost,
+          port: tunnelServerPort,
+        },
+      });
+    });
+
+    proxyServer.listen(proxyServerPort);
+
+    logger.info(`Serving UI proxy on http://localhost:${proxyServerPort}. Connect to this port to access the UI.`);
   } catch (err) {
     logger.error("Error in devServer's esbuild context");
     logger.error(err);
